664ddb67b2b8dcb3dce125fbfc1fd2f95b0f66ac
Ishagp <gisha825@gmail.com>
2025-11-13 18:29:09 +0530

    created Habitlist

--- src/components/Habitlist/Habitlist.jsx
+++ src/components/Habitlist/Habitlist.jsx
@@ -1,27 +1,37 @@
+import styles from './Habitlist.module.css';
+
 const Habitlist = ({ habits, toggleHabit }) => {
     return (
-        <div style={{ marginTop: "2rem" }}>
-            {habits.map(habit => (
-                <div
-                    key={habit.id}
-                    style={{
-                        padding: "1rem",
-                        background: habit.completed ? "#d4f8d4" : "#f2f2f2",
-                        marginBottom: "10px",
-                        borderRadius: "10px",
-                        display: "flex",
-                        justifyContent: "space-between"
-                    }}
-                >
-                    <span>{habit.title}</span>
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <h1 className={styles.heading}>Today's Habits</h1>
+                <button className={styles.add_btn}>+  Add New Habit</button>
+            </div>
 
-                    <input
-                        type="checkbox"
-                        checked={habit.completed}
-                        onChange={() => toggleHabit(habit.id)}
-                    />
-                </div>
-            ))}
+            <div style={{ marginTop: "2rem" }}>
+                {habits.map(habit => (
+                    <div
+                        key={habit.id}
+                        style={{
+                            padding: "1rem",
+                            background: habit.completed ? "#d4f8d4" : "#f2f2f2",
+                            marginBottom: "10px",
+                            borderRadius: "10px",
+                            display: "flex",
+                            justifyContent: "space-between"
+
+                        }}
+                    >
+                        <span>{habit.title}</span>
+
+                        <input
+                            type="checkbox"
+                            checked={habit.completed}
+                            onChange={() => toggleHabit(habit.id)}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
